d7183d3ee3666890bc77009158c6c8cce59f19ad
aronbarbosag <aronfbarbosa@gmail.com>
2024-03-26 13:42:49 -0300

    adicionado mais imagens total de 10 imagens diferentes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const posicoes = [0, 1, 2, 3];
+const posicoes = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+];
 
 function embaralha(lista) {
   for (let i = lista.length; i; i--) {
