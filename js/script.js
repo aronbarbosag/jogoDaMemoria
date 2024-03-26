5b7eabab8dcdc32c3da2a4081e46d5b3e1785d60
aronbarbosag <aronfbarbosa@gmail.com>
2024-03-26 01:45:35 -0300

    logica de gerar posiçao aleatoria feita mais ou menos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("eu nao cometo erros");
+const posicoes = [0, 1, 2, 3];
+
+function embaralha(lista) {
+  for (let i = lista.length; i; i--) {
+    const indiceAleatorio = Math.floor(Math.random() * i);
+
+    // guarda de um índice aleatório da lista
+    const elemento = lista[i - 1];
+
+    lista[i - 1] = lista[indiceAleatorio];
+
+    lista[indiceAleatorio] = elemento;
+  }
+  trocarImagem();
+}
+
+embaralha(posicoes);
+
+function trocarImagem() {
+  posicoes.forEach((posicao, index) => {
+    const div = document.getElementById(`${posicao}`);
+    div.querySelector("img").src = `./imagens/imagem${index}.jpg`;
+  });
+}
+console.log(posicoes);
