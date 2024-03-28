7dc2c356cd75cc01239617da56a41461d18dbed9
aronbarbosag <aronfbarbosa@gmail.com>
2024-03-28 16:05:52 -0300

    logica.txt adicionada e algoritmo para pegar o numero da imagem clicada

--- js/script.js
+++ js/script.js
@@ -23,11 +23,24 @@ function trocarImagem() {
     div.querySelector('.card-back img').src = `./imagens/imagem${index}.jpg`;
   });
 }
+let clique = 0;
 
 function handleClick(event) {
+  clique++;
   event.preventDefault();
   const target = event.currentTarget.querySelector('.card-inner');
   target.classList.add('ativo');
+  console.log(this.querySelector('.card-back img').getAttribute('src'));
+  const atributoSrc = this.querySelector('.card-back img').getAttribute('src');
+  const numeroImagem = +atributoSrc.match(/\d+/)[0];
+  let matchProximo;
+  if (numeroImagem % 2 === 0) {
+    matchProximo = numeroImagem + 1;
+  } else {
+    matchProximo = numeroImagem - 1;
+  }
+  console.log(numeroImagem);
+  console.log('proximo:', matchProximo);
 }
 
 function hiddenImg(event) {
